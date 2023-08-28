b4d2fe782f977efffd8c21324567579b43af77ed
Tarsis <br.tmvdl@gmail.com>
2023-08-28 18:45:59 -0300

    Adding "Symbol Price Ticker" getter file

---
+++ src/getters/ticker/price.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/sh
+
+# inputs
+
+path="ticker/price"
+
+symbol="${1}"
+
+if [[ -z "${symbol}" ]]
+
+then
+
+echo "symbol: $symbol"
+
+fi
+
+# runner
+
+while [[ true ]]; 
+
+do
+
+resp=$( bash binance.sh "${path}" "symbol=${symbol}" )
+
+price=$( echo "${resp}" | jq '.price' | sed -e 's/"//ig' )
+
+echo "price: ${price}"
+
+datetime=$( date +%Y%m%d%H%M%S )
+
+echo "datetime: ${datetime}"
+
+# outputs
+
+bash create.sh "${path}/${symbol}" "${datetime}" "price" "${price}"
+
+sleep .1
+
+done
