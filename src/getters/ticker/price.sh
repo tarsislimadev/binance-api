6acf9ef148befd44877c4bf86b151380a91784d5
Tarsis <br.tmvdl@gmail.com>
2023-10-18 09:57:40 -0300

    Updating scripts files

--- src/getters/ticker/price.sh
+++ src/getters/ticker/price.sh
@@ -6,34 +6,16 @@ path="ticker/price"
 
 symbol="${1}"
 
-if [[ -z "${symbol}" ]]
-
-then
-
-echo "symbol: $symbol"
-
-fi
-
 # runner
-
-while [[ true ]]; 
-
-do
 
 resp=$( bash get.sh "${path}" "symbol=${symbol}" )
 
 price=$( echo "${resp}" | jq '.price' | sed -e 's/"//ig' )
 
-echo "price: ${price}"
-
-datetime=$( date +%Y%m%d%H%M%S )
-
-echo "datetime: ${datetime}"
+. datetime.sh
 
 # outputs
 
+bash create.sh "${path}/${symbol}" "${datetime}" "resp" "${resp}"
+
 bash create.sh "${path}/${symbol}" "${datetime}" "price" "${price}"
-
-sleep .1
-
-done
