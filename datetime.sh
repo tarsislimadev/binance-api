4523ee0b66d1d7f6574a035a8fa85aa06d2a1dc1
Tarsis <br.tmvdl@gmail.com>
2023-11-04 14:35:28 -0300

    Updating script files

--- datetime.sh
+++ datetime.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/sh
 
-datetime=$( date +%Y%m%d%H%M%S )
+# yyyy-MM-dd HH:mm:ss
+
+year=$( date +%Y )
+
+month=$( date +%m )
+
+day=$( date +%d )
+
+date="${year}-${month}-${day}"
+
+hour=$( date +%H )
+
+minute=$( date +%M )
+
+second=$( date +%S )
+
+time="${hour}:${minute}:${second}"
+
+timestamp="${date} ${time}"
+
+datetime="${year}${month}${day}${hour}${minute}${second}"
