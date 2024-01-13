4cb134c6e77eadeaf49f9ca7281dfb3584befe0f
Tarsis <br.tmvdl@gmail.com>
2024-01-13 20:34:25 -0300

    Updating app files

---
+++ src/aggTrades.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# inputs
+
+. datetime.sh
+
+symbol="${1}"
+
+path="aggTrades"
+
+# runner
+
+resp=$( bash get.sh "${path}" "symbol=${symbol}" )
+
+# outputs
+
+bash create.sh "${path}" "${datetime}" "resp" "${resp}"
